ae0d20f03e47dc5428c70cd4078a7e8742742893
Alex <alex.picault@studocu.com>
2022-01-08 17:49:55 +0100

    Generate WOD when bot is mentioned

--- app.js
+++ app.js
@@ -6,25 +6,21 @@ const wordList = require('./wordList.js').wordList;
 
 const app = new App({
     token: process.env.TOKEN,
-    signingSecret: process.env.SIGNIN_SECRET
+    signingSecret: process.env.SIGNIN_SECRET,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
 });
 
-async function sendWordOfTheDay() {
-    const word = await getWordOfTheDay();
-    const gif = await getGif(word);
-    // C02EBT77QH1 community private id
-    // CB3G48RNJ fakenews channel test id
-    await publishMessage('CB3G48RNJ', gif, word)
-}
+const capitalize = s => (s && s[0].toUpperCase() + s.slice(1)) || "";
 
 async function getWordOfTheDay() {
-    const now = new Date();
-    const start = new Date(now.getFullYear(), 0, 0);
-    const diff = now - start;
-    const oneDay = 1000 * 60 * 60 * 24;
-    const day = Math.floor(diff / oneDay);
-    // const wordListIndex = Math.floor(Math.random() * 1491) + 1;
-    return wordList[day];
+    // const now = new Date();
+    // const start = new Date(now.getFullYear(), 0, 0);
+    // const diff = now - start;
+    // const oneDay = 1000 * 60 * 60 * 24;
+    // const day = Math.floor(diff / oneDay);
+    const wordListIndex = Math.floor(Math.random() * 1491) + 1;
+    return wordList[wordListIndex];
 }
 
 async function getGif(wordOfTheDay) {
@@ -44,28 +40,60 @@ async function getGif(wordOfTheDay) {
         });
 }
 
-const capitalize = s => (s && s[0].toUpperCase() + s.slice(1)) || "";
+function formatMessage(gif, word) {
+    return [
+        {
+            type: 'section',
+            text: {
+                type: 'plain_text',
+                text: capitalize(word)
+            },
+            accessory: {
+                type: "button",
+                text: {
+                    type: "plain_text",
+                    text: "switch word",
+                    emoji: true
+                },
+                value: "click_me_123",
+                action_id: "switch_word"
+            }
+        },
+        {
+            type: 'image',
+            image_url: gif,
+            alt_text: 'Yay! The modal was updated'
+        },
+    ]
+}
 
-async function publishMessage(id, gif, word) {
+async function deleteMessage(channelId, messageId) {
+    try {
+        await app.client.chat.delete({
+            channel: channelId,
+            ts: messageId
+        });
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+async function sendWordOfTheDay(say) {
+    const word = await getWordOfTheDay();
+    const gif = await getGif(word);
+    await say({"blocks": formatMessage(gif, word)});
+}
+
+async function publishNewWOD(channelId) {
+    const word = await getWordOfTheDay();
+    const gif = await getGif(word);
     //https://www.npmjs.com/package/node-schedule
     try {
         await app.client.chat.postMessage({
             token: process.env.TOKEN,
-            channel: id,
-            blocks: [
-                {
-                    type: 'header',
-                    text: {
-                        type: 'plain_text',
-                        text: capitalize(word)
-                    }
-                },
-                {
-                    type: 'image',
-                    image_url: gif,
-                    alt_text: 'Yay! The modal was updated'
-                }
-            ]
+            channel: channelId,
+            blocks: formatMessage(gif, word)
         });
     }
     catch (error) {
@@ -76,8 +104,26 @@ async function publishMessage(id, gif, word) {
 (async () => {
     await app.start(process.env.PORT || 3000);
     console.log('⚡️ Bolt app is running!');
-    await sendWordOfTheDay();
-    process.exit(1)
+
+    app.event('app_mention', async ({ event, context, client, say}) => {
+        try {
+            await sendWordOfTheDay(say);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+
+    app.action('switch_word', async (event) => {
+        try {
+            const container = event.body.container;
+            await deleteMessage(container.channel_id, container.message_ts);
+            await publishNewWOD(container.channel_id);
+        } catch (error) {
+            console.error(error);
+        }
+    });
+    // process.exit(1)
 })();
 
 
