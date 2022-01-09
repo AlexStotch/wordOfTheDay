6ca0b67bf1f5ab8385aea352c1f46e24583d48b4
Alex <alex.picault@studocu.com>
2022-01-09 12:51:43 +0100

    feat: update format message

--- formatMessage.js
+++ formatMessage.js
@@ -1,28 +1,56 @@
-const capitalize = s => (s && s[0].toUpperCase() + s.slice(1)) || "";
+const capitalize = s => (s && s[0].toUpperCase() + s.slice(1)) || '';
 
 export function formatMessage(gif, word) {
     return [
         {
-            type: 'section',
+            type: 'divider',
+            block_id: 'divider1'
+        },
+        {
+            type: 'header',
             text: {
                 type: 'plain_text',
                 text: capitalize(word)
             },
-            accessory: {
-                type: "button",
-                text: {
-                    type: "plain_text",
-                    text: "switch word",
-                    emoji: true
-                },
-                value: "click_me_123",
-                action_id: "switch_word"
-            }
         },
         {
             type: 'image',
             image_url: gif,
             alt_text: 'Yay! The modal was updated'
+        },
+        {
+            type: 'divider',
+            block_id: 'divider2'
+        },
+        {
+            type: 'actions',
+            elements: [
+                {
+                    type: 'button',
+                    text: {
+                        type: 'plain_text',
+                        text: 'Switch Word',
+                        emoji: true
+                    },
+                    value: 'click_me_123',
+                    action_id: "switch_word"
+                }
+            ]
+        },
+        {
+            type: 'actions',
+            elements: [
+                {
+                    type: 'button',
+                    text: {
+                        type: 'plain_text',
+                        text: 'Switch Gif',
+                        emoji: true
+                    },
+                    value: "click_me_1234",
+                    action_id: "switch_gif"
+                }
+            ]
         },
     ]
 }
