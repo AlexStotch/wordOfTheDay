e8c72edae276d605973ecc0346f10ffaec88ff41
Alex <alex.picault@studocu.com>
2022-01-16 21:47:39 +0100

    feat: getRandomWOD in webapp server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
-const express = require('express');
+import express from 'express';
+import getRandomWOD from '../slack/utils/getRandomWOD.js';
 
 const app = express();
 const PORT = 3001;
 
-app.get('/word', (request, response) => {
+app.get('/word', async (request, response) => {
+  const word = await getRandomWOD();
   response.header('Access-Control-Allow-Origin', '*');
-  response.send({ word: 'pate' });
+  response.send({ word });
 });
 
 app.listen(PORT, () => {
